64e3e7ad1f17781f8ea3c6f0df765049e179c3a5
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-01-13 11:26:57 -0300

    Criando a conecção com o mongodb
    usando mongoose para conectar ao banco

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const DevController = require('./controllers/DevController')
+const LikeController = require('./controllers/LikeController')
 
 const routes = express.Router()
 //GET, POST, PUT, DELETE
@@ -11,4 +13,6 @@ routes.post('/devs', (req, res) => {
     return res.json(req.body)
 })
 
+routes.post('/devs', DevController.store)
+routes.post('/devs/:devId/likes', LikeController.store)
 module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const app = express()
 
+mongoose.connect('mongodb+srv://userAdmin:adminUser@cluster0-mzpo4.mongodb.net/bd-omnistack8?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(express.json())
 app.use(routes)
 
