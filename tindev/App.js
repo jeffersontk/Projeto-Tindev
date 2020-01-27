992a20078332bea9bca7e87ccb72c8ac18f5aeeb
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-01-27 19:26:29 -0300

    Pagina de Login

--- tindev/App.js
+++ tindev/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Login from './src/pages/Login'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>HELLO DEV</Text>
-    </View>
+    <Login />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
