8b7a0a9686506cf175e419a4333775b9df3d0fe2
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-01-28 15:01:17 -0300

    tirando os botões caso não tenha um card de usuario

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -70,18 +70,20 @@ export default function Main({ navigation }) {
                     )}
 
             </View>
-            <View style={styles.buttonsContainer}>
-                <TouchableOpacity
-                    onPress={handleDislike}
-                    style={styles.button}>
-                    <Image source={dislike} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={handleLike}
-                    style={styles.button}>
-                    <Image source={like} />
-                </TouchableOpacity>
-            </View>
+            {users.length > 0 && (
+                <View style={styles.buttonsContainer}>
+                    <TouchableOpacity
+                        onPress={handleDislike}
+                        style={styles.button}>
+                        <Image source={dislike} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={handleLike}
+                        style={styles.button}>
+                        <Image source={like} />
+                    </TouchableOpacity>
+                </View>
+            )}
         </SafeAreaView>)
 }
 
