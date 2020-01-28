a5fadd3ad6a8281d278196f7fcb2f3fad04658f2
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-01-27 23:26:15 -0300

    criando a aplicação mobile pt-1
    criando as rotas e os componetes login e main
    obs o Async storage está dando problema

--- tindev/App.js
+++ tindev/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Login from './src/pages/Login'
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <Login />
+    <Routes />
   );
 }
--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+//import AsyncStorage from '@react-native-community/async-storage'
 import { KeyboardAvoidingView, Platform, StyleSheet, Image, TextInput, TouchableOpacity, Text } from 'react-native'
+
+import api from '../services/api'
 
 import logo from '../assets/logo.png'
 
-export default function Login() {
+export default function Login({ navigation }) {
+
+    const [user, setUser] = useState('')
+
+    /*  useEffect(() => {
+         AsyncStorage.getItem('user').then(user => {
+             if (user) {
+                 navigation.navigate('Main', { user })
+             }
+         })
+     }, []) */
+
+    async function handleLogin() {
+
+        const response = await api.post('/devs', {
+            username: user
+        })
+        const { _id } = response.data
+
+        //await AsyncStorage.setItem('user', _id);
+
+        navigation.navigate('Main', { _id })
+
+    }
     return (
         <KeyboardAvoidingView
             behavior="padding"
@@ -19,8 +45,10 @@ export default function Login() {
                 autoCapitalize="none"
                 autoCorrect={false}
                 style={styles.input}
+                value={user}
+                onChangeText={setUser}
             />
-            <TouchableOpacity style={styles.button} >
+            <TouchableOpacity onPress={handleLogin} style={styles.button} >
                 <Text style={styles.buttonText}>Entrar</Text>
             </TouchableOpacity>
         </KeyboardAvoidingView>
@@ -58,5 +86,6 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 20
 
-    }
+    },
+
 })
